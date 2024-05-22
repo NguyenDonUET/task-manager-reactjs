aa53d95f8602af041d03cba17a45566b74011d59
NguyenDonUET <nguyendoncb@gmail.com>
2024-05-22 12:16:59 +0700

    feat: install react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
 import CssBaseline from "@mui/material/CssBaseline"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
@@ -10,12 +11,16 @@ import { RouterProvider } from "react-router-dom"
 import "./index.css"
 import { store } from "./redux/store"
 import router from "./router"
+// Create a client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 )
