07907ec3ea5cb16d9ed467c83b2b74ee1fcd7061
NguyenDonUET <nguyendoncb@gmail.com>
2024-05-23 17:13:27 +0700

    feat(ui) add some styles to task card

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,26 +1,64 @@
 // TaskCard.tsx
 
 import React from "react"
-import { Card, CardContent, Typography } from "@mui/material"
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material"
 import { TaskType } from "../../types"
-
+import { Pencil, Trash2 } from "lucide-react"
 interface TaskCardProps {
   task: TaskType
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
   return (
-    <Card className='min-h-[250px]'>
+    <Card
+      variant='outlined'
+      sx={{ minHeight: "300px", display: "flex", flexDirection: "column" }}
+      className='shadow-lg'
+    >
       <CardContent>
-        <Typography variant='h6'>{task.name}</Typography>
+        <Typography variant='h6' color={"#673ab7"}>
+          {task.name}
+        </Typography>
         <Typography variant='body1'>Description: {task.description}</Typography>
         <Typography variant='body2'>Priority: {task.priority}</Typography>
         <Typography variant='body2'>Status: {task.status}</Typography>
         <Typography variant='body2'>Created at: {task.createdAt}</Typography>
         <Typography variant='body2'>Updated at: {task.updatedAt}</Typography>
         <Typography variant='body2'>Deadline: {task.deadline}</Typography>
-        <Typography variant='body2'>User ID: {task.userId}</Typography>
       </CardContent>
+      <CardActions disableSpacing className='justify-between mt-auto'>
+        <Button
+          variant='contained'
+          sx={{
+            borderRadius: "16px",
+            marginLeft: "2px",
+            textTransform: "capitalize",
+            backgroundColor: "#4caf50",
+            "&:hover": {
+              backgroundColor: "#357a38",
+            },
+          }}
+        >
+          Mark as done
+        </Button>
+        <Stack direction={"row"}>
+          <IconButton aria-label='delete' size='large'>
+            <Pencil color='#38a56a' />
+          </IconButton>
+          <IconButton aria-label='delete' size='large'>
+            <Trash2 color='#d32f2f' />
+          </IconButton>
+        </Stack>
+      </CardActions>
     </Card>
   )
 }
