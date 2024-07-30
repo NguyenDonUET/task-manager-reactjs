4f085fcdd8f2e245062c422ecad9faaaf901fc9b
NguyenDonUET <nguyendoncb@gmail.com>
2024-07-30 22:52:33 +0700

    refactor: remove unused function

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,21 +1,8 @@
 import axios from 'axios'
 import { UserType } from '../redux/user/userSlice'
 import { SignInFormData, SignUpFormData } from '../types'
-import { BACKEND_URL, LOCAL_ACCESS_TOKEN_KEY } from '../utils/constants'
+import { BACKEND_URL } from '../utils/constants'
 import { axiosInstance } from './customAxios'
-
-export const refreshToken = async () => {
-  try {
-    const { data } = await axios.get(`${BACKEND_URL}/auth/refresh-token`, {
-      withCredentials: true,
-    })
-    const newAccessToken = data.accessToken
-    localStorage.setItem(LOCAL_ACCESS_TOKEN_KEY, newAccessToken)
-  } catch (error) {
-    // refresh token hết hạn
-    console.log('🚀 ~ refreshToken ~ error:', error)
-  }
-}
 
 export const getUser = async () => {
   const { data } = await axiosInstance.get<{ user: UserType }>(`/users/account`)
